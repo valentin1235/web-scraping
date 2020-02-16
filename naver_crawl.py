7507c5760eaf7d071506a7f229ca8c8c59c75652
valentin1235 <valentin123@naver.com>
2020-02-16 14:17:47 +0900

    naver crawl simple func

--- naver_crawl.py
+++ naver_crawl.py
@@ -2,34 +2,29 @@ import urllib.request
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://search.naver.com/search.naver?sm=tab_hty.top&where=news&query=python&oquery=%ED%8C%8C%EC%9D%B4%EC%8D%AC&tqi=UC4jcsprvTossThcjQZssssssT0-093442'
 
 
+def naver_crawl(url):
+    html = requests.get(url).text
+    soup = BeautifulSoup(html, 'html.parser')
 
-html = requests.get(url).text
+    title=[]
+    linked_url=[]
+    result=[]
 
-soup = BeautifulSoup(html, 'html.parser')
+    a=soup.find_all(class_="_sp_each_url _sp_each_title")
+    for i in a:
+        title.append(i.attrs['title'])
 
+    for i in a:
+        linked_url.append(i.attrs['href'])
 
-
-
-title=[]
-linked_url=[]
-result=[]
-
-a=soup.find_all(class_="_sp_each_url _sp_each_title")
-for i in a:
-    title.append(i.attrs['title'])
-
-for i in a:
-    linked_url.append(i.attrs['href'])
-
-for i in zip(title,linked_url):
-    result.append(
-        {
+    for i in zip(title,linked_url):
+        result.append(
+            {
             'title' : i[0],
             'url'   : i[1]
-        }
-    )
-
-print(result)
+            }
+        )
+    return result
+print(naver_crawl('https://search.naver.com/search.naver?sm=tab_hty.top&where=news&query=python&oquery=%ED%8C%8C%EC%9D%B4%EC%8D%AC&tqi=UC4jcsprvTossThcjQZssssssT0-093442'))
